0a72c772686b526f4a58dc7ed7e3748952434854
Jamie Burns <jamie.burns@awin.com>
2018-04-04 12:20:02 +0100

    Reflections with shaded Guava dependency

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reflections Shaded to avoid Guava conflicts
+
+See original: https://github.com/ronmamo/reflections
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Noop
+ */
+public class App
+{
+    public static void main(
+        String[] args
+    )
+    {
+        System.out.println("Noop");
+    }
+}
